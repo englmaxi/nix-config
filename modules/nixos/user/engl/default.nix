bc9a54f9f46df224f1ea24659debcbc5724e42ba
englmaxi <engl@disroot.org>
2024-11-19 23:42:16 +0100

    add user age key

--- modules/nixos/user/engl/default.nix
+++ modules/nixos/user/engl/default.nix
@@ -7,12 +7,19 @@
 in {
   home-manager.users.${userName} = import (../../../../home + "/${userName}/${config.networking.hostName}.nix");
 
-  sops.secrets."hashed_passwords/${userName}".neededForUsers = true;
+  sops.secrets = {
+    "hashed_passwords/${userName}".neededForUsers = true;
+    "user_age_keys/${userName}_${config.networking.hostName}" = {
+      owner = config.users.users.${userName}.name;
+      inherit (config.users.users.${userName}) group;
+      path = "/home/${userName}/.config/sops/age/keys.txt";
+    };
+  };
 
   users = {
     mutableUsers = false;
 
-    users.engl = {
+    users.${userName} = {
       isNormalUser = true;
       extraGroups = ["wheel"];
       home = "/home/${userName}";
