dd091d1efec0582b18b9a1dd801724370e813e20
englmaxi <engl@disroot.org>
2024-12-01 17:11:55 +0100

    update

---
+++ hosts/pc/default.nix
@@ -0,0 +1,52 @@
+{
+  inputs,
+  pkgs,
+  ...
+}: let
+  hostName = "pc";
+in {
+  imports = [
+    # modules
+    inputs.nixos-hardware.nixosModules.common.pc.ssd
+    inputs.nixos-hardware.nixosModules.common.cpu.haswell
+    inputs.nixos-hardware.nixosModules.common.gpu.nvidia.ampere
+    inputs.disko.nixosModules.disko
+
+    # hardware
+    ./disks.nix
+    ./hardware-configuration.nix
+
+    #core
+    ../../modules/nixos/core
+
+    # optional
+    ../../modules/nixos/optional/desktop/gnome.nix
+    ../../modules/nixos/optional/impermanence.nix
+    ../../modules/nixos/optional/theme/catppuccin-mocha
+    ../../modules/nixos/optional/yubikey.nix
+
+    # users
+    ../../modules/nixos/user/engl
+  ];
+
+  modules.nixos = {
+    core = {
+      locales.keyMap = "de";
+    };
+  };
+
+  networking = {
+    inherit hostName;
+    networkmanager.enable = true;
+  };
+
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+  ];
+}
